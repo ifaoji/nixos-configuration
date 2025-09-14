3c21d39005f0a51696c9172d247239d592e6c3eb
Simon Dablander <ifaoji@pm.me>
2025-09-14 19:34:03 +0200

    fix: add sha265 to homemanager tarball fetch

--- home-manager.nix
+++ home-manager.nix
@@ -1,6 +1,9 @@
 { username, ... }:
 let
-  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz";
+  home-manager = builtins.fetchTarball {
+    url = "https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz";
+    sha256 = "02j4a4df9z2zk95d985vcwb5i4vdriyrkx61ah9xwqyqjciw98rb";
+  };
 in
 {
   imports =
