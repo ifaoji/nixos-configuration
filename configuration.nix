40b4bb5ee8c6171da8d5acc1a8446e1edad2a025
Simon Dablander <ifaoji@pm.me>
2025-09-14 19:10:29 +0200

    fix: allow connection from browser to 1password

--- configuration.nix
+++ configuration.nix
@@ -185,6 +185,14 @@
 
   virtualisation.docker.enable = true;
 
+  programs._1password.enable = true;
+  programs._1password-gui = {
+    enable = true;
+    # Certain features, including CLI integration and system authentication support,
+    # require enabling PolKit integration on some desktop environments (e.g. Plasma).
+    polkitPolicyOwners = [ "phi" ];
+  };
+
   environment.etc = {
       "1password/custom_allowed_browsers" = {
         text = ''
